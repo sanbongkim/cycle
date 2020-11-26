6c8f1c3aaaafff447e8a98bd58bddfb4fa2a50e4
SSG <ksb700djr@naver.com>
2020-11-26 10:01:28 +0900

    Start

---
+++ cycle/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  cycle
+//
+//  Created by SSG on 2020/11/23.
+//
+
+import UIKit
+import SideMenu
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        var menu:SideMenuNavigationController?
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //menu = SideMenuNavigationController(rootViewController:)
+    }
+}
+
